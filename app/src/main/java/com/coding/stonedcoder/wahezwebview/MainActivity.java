7e2734d0c958ffa9622e16d0866f8cc24e37d85a
stonedcoder <vraunaq1213@ghmail.com>
2018-01-29 10:34:10 +0530

    Permissions added.

--- app/src/main/java/com/coding/stonedcoder/wahezwebview/MainActivity.java
+++ app/src/main/java/com/coding/stonedcoder/wahezwebview/MainActivity.java
@@ -1,13 +1,34 @@
 package com.coding.stonedcoder.wahezwebview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = new WebView(this);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        webView.loadUrl("https://wahez.com/");
+        setContentView(webView);
     }
 }
